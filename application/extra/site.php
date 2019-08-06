0ea0042cc49b9542947cd86788beede87dfb20b4
xcxlegend <xcx_legender@qq.com>
2019-08-06 22:02:46 +0800

    init

---
+++ application/extra/site.php
@@ -0,0 +1,34 @@
+<?php
+
+return array (
+  'name' => '话充网厅平台',
+  'beian' => '',
+  'cdnurl' => '',
+  'version' => '1.0.1',
+  'timezone' => 'Asia/Shanghai',
+  'forbiddenip' => '',
+  'languages' => 
+  array (
+    'backend' => 'zh-cn',
+    'frontend' => 'zh-cn',
+  ),
+  'fixedpage' => 'dashboard',
+  'categorytype' => 
+  array (
+    'default' => 'Default',
+  ),
+  'configgroup' => 
+  array (
+    'basic' => 'Basic',
+    'email' => 'Email',
+    'dictionary' => 'Dictionary',
+    'user' => 'User',
+  ),
+  'mail_type' => '1',
+  'mail_smtp_host' => 'smtp.qq.com',
+  'mail_smtp_port' => '465',
+  'mail_smtp_user' => '10000',
+  'mail_smtp_pass' => 'password',
+  'mail_verify_type' => '2',
+  'mail_from' => '10000@qq.com',
+);
---
+++ application/index/controller/Index.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\index\controller;
+
+use app\common\controller\Frontend;
+
+class Index extends Frontend
+{
+
+    protected $noNeedLogin = '*';
+    protected $noNeedRight = '*';
+    protected $layout = '';
+
+    public function index()
+    {
+        $this->redirect("index/user/index");
+        return;
+        return $this->view->fetch();
+    }
+
+    public function news()
+    {
+        $newslist = [];
+        return jsonp(['newslist' => $newslist, 'new' => count($newslist), 'url' => 'https://www.fastadmin.net?ref=news']);
+    }
+
+}
